09091e79fcb306186697f48568c3d72e0a7e006c
Заур <zaurinio@inbox.ru>
2024-11-04 19:26:06 +0300

    fix: change self-closing tags

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    plugins: ["@awmottaz/prettier-plugin-void-html"]
+};
+
+export default config;
